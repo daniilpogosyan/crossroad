fa23ea35e3170181b76d071242eabb6970603047
Daniil Pogosyan <daniilpogosyan@gmail.com>
2024-11-13 13:50:50 +0000

    Add smooth car movement

--- src/components/car/car.ts
+++ src/components/car/car.ts
@@ -2,22 +2,34 @@ import { CAR_WIDTH } from "../../constants/index";
 import { Position } from "../../types/position";
 import { RoadNode } from "../road/road-node/road-node";
 import { getDistance, getNormalizedVector } from "../utils/get-random-color";
-
+// (.)(.)
 export class Car {
+  private from: RoadNode;
   private to: RoadNode;
   public position: Position;
 
-  constructor(public speed: number, from: RoadNode, to: RoadNode) {
-    this.position = from.getPosition();
+  constructor(
+    public speed: (distance: number, distanceLeft: number) => number,
+    from: RoadNode,
+    to: RoadNode
+  ) {
+    this.from = from;
     this.to = to;
+    this.position = from.getPosition();
   }
 
   move() {
+    const fromPosition = this.from.getPosition();
     const toPosition = this.to.getPosition();
 
     const { x, y } = getNormalizedVector(this.position, toPosition);
-    this.position.x += x * this.speed;
-    this.position.y += y * this.speed;
+    const currentSpeed = this.speed(
+      getDistance(fromPosition, toPosition),
+      getDistance(this.position, toPosition)
+    );
+    console.log(`L30`, currentSpeed);
+    this.position.x += x * currentSpeed;
+    this.position.y += y * currentSpeed;
   }
 
   getDestination() {
@@ -25,6 +37,7 @@ export class Car {
   }
 
   place(from: RoadNode, to: RoadNode) {
+    this.from = from;
     this.to = to;
     const startPosition = from.getPosition();
     this.position = startPosition;
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ const nodes = [
   new RoadNode({ x: 100, y: 100 }),
   new RoadNode({ x: 100, y: 200 }),
   new RoadNode({ x: 50, y: 200 }),
+  new RoadNode({ x: 200, y: 200 }),
 ];
 nodes[0].addNeighbor(nodes[1]);
 nodes[1].addNeighbor(nodes[2]);
@@ -19,6 +20,8 @@ nodes[2].addNeighbor(nodes[3]);
 nodes[3].addNeighbor(nodes[4]);
 nodes[4].addNeighbor(nodes[5]);
 nodes[5].addNeighbor(nodes[0]);
+nodes[2].addNeighbor(nodes[6]);
+nodes[6].addNeighbor(nodes[5]);
 
 for (const node of nodes) {
   console.log(`L22`, node.getNeighbors().length, node.getPosition());
@@ -31,27 +34,41 @@ const graphView = new RoadGraphView(graph);
 
 // graphView.draw();
 
-const car = new Car(2, nodes[0], nodes[1]);
-car.place(nodes[0], nodes[1]);
-const carView = new CarView(car);
-// carView.draw();
+function speedFunction(distance: number, distanceLeft: number) {
+  return Math.sin((distanceLeft / distance) * Math.PI) + 0.1;
+}
+
+const cars = [
+  new Car(speedFunction, nodes[0], nodes[1]),
+  new Car(speedFunction, nodes[1], nodes[2]),
+  new Car(speedFunction, nodes[1], nodes[3]),
+  new Car(speedFunction, nodes[4], nodes[5]),
+];
+
+const carViews = cars.map((car) => new CarView(car));
 
 function draw() {
-  car.move();
+  graphView.draw();
 
-  if (car.hasArrived()) {
-    const destination = car.getDestination();
-    const destinationNeighbors = destination.getNeighbors();
-    console.log(`L38`, destinationNeighbors);
-    const nextDestination =
-      destinationNeighbors[
-        Math.floor(destinationNeighbors.length * Math.random())
-      ];
-    car.place(destination, nextDestination.node);
+  for (let i = 0; i < cars.length; i++) {
+    const car = cars[i];
+    const carView = carViews[i];
+    car.move();
+
+    if (car.hasArrived()) {
+      const destination = car.getDestination();
+      const destinationNeighbors = destination.getNeighbors();
+      console.log(`L38`, destinationNeighbors);
+      const nextDestination =
+        destinationNeighbors[
+          Math.floor(destinationNeighbors.length * Math.random())
+        ];
+      car.place(destination, nextDestination.node);
+    }
+
+    carView.draw();
   }
 
-  graphView.draw();
-  carView.draw();
   // return;
 
   requestAnimationFrame(draw);
