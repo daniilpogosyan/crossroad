3486b391cb8c2888ab196394932b83b112fd72c3
Daniil Pogosyan <daniilpogosyan@gmail.com>
2024-11-17 10:29:34 +0000

    Make car speed a function

--- src/components/car/car.ts
+++ src/components/car/car.ts
@@ -1,36 +1,71 @@
-import { CAR_WIDTH } from "../../constants/index";
 import { Position } from "../../types/position";
 import { RoadNode } from "../road/road-node/road-node";
 import { getDistance, getNormalizedVector } from "../utils/get-random-color";
 
 export class Car {
   private to: RoadNode;
+  private from: RoadNode;
   public position: Position;
+  private speed: (car: Car) => number;
 
-  constructor(public speed: number, from: RoadNode, to: RoadNode) {
+  constructor(speed: (car: Car) => number, from: RoadNode, to: RoadNode);
+  constructor(speed: number, from: RoadNode, to: RoadNode);
+  constructor(
+    speed: ((car: Car) => number) | number,
+    from: RoadNode,
+    to: RoadNode
+  ) {
     this.position = from.getPosition();
     this.to = to;
+    this.from = from;
+
+    this.speed = typeof speed === "function" ? speed : () => speed * 0.2;
   }
 
   move() {
     const toPosition = this.to.getPosition();
 
     const { x, y } = getNormalizedVector(this.position, toPosition);
-    this.position.x += x * this.speed;
-    this.position.y += y * this.speed;
+
+    const dx =
+      Math.sign(x) *
+      Math.min(
+        Math.abs(x * this.speed(this)),
+        Math.abs(toPosition.x - this.position.x)
+      );
+
+    const dy =
+      Math.sign(y) *
+      Math.min(
+        Math.abs(y * this.speed(this)),
+        Math.abs(toPosition.y - this.position.y)
+      );
+
+    this.position.x += dx;
+    this.position.y += dy;
   }
 
   getDestination() {
     return this.to;
   }
 
+  getStart() {
+    return this.from;
+  }
+
   place(from: RoadNode, to: RoadNode) {
     this.to = to;
+    this.from = from;
+
     const startPosition = from.getPosition();
     this.position = startPosition;
   }
 
   hasArrived() {
-    return getDistance(this.position, this.to.getPosition()) < CAR_WIDTH / 10;
+    return getDistance(this.position, this.to.getPosition()) < 1e-5;
+  }
+
+  getRoad() {
+    return this.from.getNeighbors().find(({ node }) => node === this.to);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { CarView } from "./components/car/car.view";
 import { RoadGraph } from "./components/road/road-graph/road-graph";
 import { RoadGraphView } from "./components/road/road-graph/road-graph.view";
 import { RoadNode } from "./components/road/road-node/road-node";
+import { getDistance } from "./components/utils/get-random-color";
 
 const nodes = [
   new RoadNode({ x: 50, y: 50 }),
@@ -20,39 +21,49 @@ nodes[3].addNeighbor(nodes[4]);
 nodes[4].addNeighbor(nodes[5]);
 nodes[5].addNeighbor(nodes[0]);
 
-for (const node of nodes) {
-  console.log(`L22`, node.getNeighbors().length, node.getPosition());
-}
-
 const graph = new RoadGraph(nodes[0]);
-
-console.log(`L29`, graph.getAllEdgesAndNodes());
 const graphView = new RoadGraphView(graph);
 
 // graphView.draw();
 
-const car = new Car(2, nodes[0], nodes[1]);
+const car = new Car(
+  (car) => {
+    const distance = getDistance(
+      car.getStart().getPosition(),
+      car.getDestination().getPosition()
+    );
+    const distanceLeft = getDistance(
+      car.position,
+      car.getDestination().getPosition()
+    );
+    return Math.sin((distanceLeft / distance) * Math.PI) + 0.1;
+  },
+  nodes[0],
+  nodes[1]
+);
 car.place(nodes[0], nodes[1]);
 const carView = new CarView(car);
-// carView.draw();
 
 function draw() {
   car.move();
 
   if (car.hasArrived()) {
     const destination = car.getDestination();
-    const destinationNeighbors = destination.getNeighbors();
-    console.log(`L38`, destinationNeighbors);
+    const start = car.getStart();
+    const destinationNeighborsWithoutStart = destination
+      .getNeighbors()
+      .filter((x) => x.node !== start);
+
     const nextDestination =
-      destinationNeighbors[
-        Math.floor(destinationNeighbors.length * Math.random())
-      ];
-    car.place(destination, nextDestination.node);
+      destinationNeighborsWithoutStart[
+        Math.floor(destinationNeighborsWithoutStart.length * Math.random())
+      ]?.node ?? start;
+
+    car.place(destination, nextDestination);
   }
 
   graphView.draw();
   carView.draw();
-  // return;
 
   requestAnimationFrame(draw);
 }
